edd97644479093691047c730b4542d3452781706
jakego1 <jakego13129@gmail.com>
2025-05-14 10:16:33 +0900

    commit

--- src/main/java/com/jakego/ebproj/dto/MenuDTO.java
+++ src/main/java/com/jakego/ebproj/dto/MenuDTO.java
@@ -1,4 +1,13 @@
 package com.jakego.ebproj.dto;
 
+import lombok.Data;
+
+@Data
 public class MenuDTO {
+
+    private int menuCode;
+    private String menuName;
+    private String menuPrice;
+    private String categoryCode;
+    private String orderableStatus;
 }
--- src/main/java/com/jakego/ebproj/service/MenuService.java
+++ src/main/java/com/jakego/ebproj/service/MenuService.java
@@ -1,4 +1,27 @@
 package com.jakego.ebproj.service;
 
+import com.jakego.ebproj.dto.MenuDTO;
+import com.jakego.ebproj.entity.Menu;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class MenuService {
+
+    private final MenuRepository menuRepository;
+    private final ModelMapper modelMapper;
+
+    @Autowired
+    public MenuService(MenuRepository menuRepository, ModelMapper modelMapper) {
+        this.menuRepository = menuRepository;
+        this.modelMapper = modelMapper;
+    }
+
+
+    public MenuDTO findMenuByMenuCode(int menuCode) {
+        Menu selectedMenu = menuRepository.findById(menuCode).get();
+
+        return modelMapper.map(selectedMenu, MenuDTO.class);
+    }
 }
