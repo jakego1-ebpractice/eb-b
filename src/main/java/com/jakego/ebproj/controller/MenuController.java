976e8381c871a50a21b77896e5a51645dcc5c256
jakego1 <jakego13129@gmail.com>
2025-05-14 10:01:30 +0900

    a

--- src/main/java/com/jakego/ebproj/controller/MenuController.java
+++ src/main/java/com/jakego/ebproj/controller/MenuController.java
@@ -3,6 +3,7 @@ package com.jakego.ebproj.controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+//
 @RestController
 public class MenuController {
 
